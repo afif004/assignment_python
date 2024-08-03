72adcc604630171860bb4f8d8e335f7f6a737537
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-04 00:31:28 +0600

    Delete If-Else directory

--- If-Else/char_type.py
+++
@@ -1,9 +0,0 @@
-C = input()
-if C>='0' and C<='9':
-    print("Digit")
-elif C==' ':
-    print("Space")
-elif ((C>='A' and C<='Z') or (C>='a' and C<='z')):
-    print("Albhabet")
-else:
-    print("Punctuation")
