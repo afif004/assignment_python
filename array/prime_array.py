1a37c2637cc0c0bd9ed141eabb2d3e3cc925c13f
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-05 03:56:29 +0600

    Update prime_array.py

--- array/prime_array.py
+++ array/prime_array.py
@@ -1,5 +1,6 @@
+import math
 def is_prime(n):
-    return all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))
+    return all(n % i != 0 for i in range(2, int(math.sqrt(n)) + 1))
 
 def generate_primes(limit):
     primes = []
