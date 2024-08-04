89e36b6dc926a186aa90bada588bc00009cfef85
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-05 04:11:12 +0600

    Update perfect_arr.py

--- Functions/array/perfect_arr.py
+++ Functions/array/perfect_arr.py
@@ -9,17 +9,19 @@ def perfect_number(n):
         return perfect
     return None
 
+def perfect_num_generate(N):
+    perfect_numbers = []
+    n = 2
+    pnum_count = 0
+    while pnum_count < N:
+        perfect_num = perfect_number(n)
+        if perfect_num:
+            perfect_numbers.append(perfect_num)
+            pnum_count += 1
+        n += 1
+    return perfect_numbers
+
 N = int(input("Number of perfect numbers to generate: "))
-perfect_numbers = []
-n = 2
-pnum_count = 0
-
-while pnum_count < N:
-    perfect_num = perfect_number(n)
-    if perfect_num:
-        perfect_numbers.append(perfect_num)
-        pnum_count += 1
-    n += 1
-
+perfect_numbers = perfect_num_generate(N)
 print(f"Perfect number: {perfect_numbers[N-1]}")
 print("All perfect numbers generated:", perfect_numbers)
