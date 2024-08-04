d59fe61f53f0b5185d8a745356683b4100da5951
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-05 04:01:08 +0600

    add prime_array

---
+++ Functions/array/prime_arr.py
@@ -0,0 +1,17 @@
+import math
+def is_prime(n):
+    return all(n % i != 0 for i in range(2, int(math.sqrt(n)) + 1))
+
+def generate_primes(limit):
+    primes = []
+    num = 2
+    while len(primes) < limit:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+N = int(input("Number of primes: "))
+primes = generate_primes(N)
+print(f"Prime array: {primes}")
+print(primes[N-1])
