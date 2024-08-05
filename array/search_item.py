b365ecbb72c18d39dcce3039878b6e19baf5a564
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-06 01:42:02 +0600

    Update search_item.py

--- array/search_item.py
+++ array/search_item.py
@@ -1,14 +1,14 @@
 N = int(input())
 arr =[]
-for i in range(0,N):
+for i in range(N):
     arr.append(int(input()))
 item = int(input("Search: "))
 found = 0
 
-for i in range(0,N):
+for i in range(N):
     if arr[i] == item:
         print(f"Item found at location: {i}")
-        found = True
+        found = 1
         break
 if not found:
     print("ITEM Not Found")
