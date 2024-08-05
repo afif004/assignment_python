f350a8a2f2711084a29f18c899766d3c9454b335
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-06 02:03:44 +0600

    Update array

---
+++ Functions/array/sum_mat.py
@@ -0,0 +1,23 @@
+def read_matrix(size, name):
+    matrix = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(int(input(f"{name}[{i}][{j}]: ")))
+        matrix.append(row)
+    return matrix
+
+def sum_matrices(A, B, size):
+    C = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            C[i][j] = A[i][j] + B[i][j]
+    return C
+
+size = int(input("Size of matrix: "))
+A = read_matrix(size, 'A')
+B = read_matrix(size, 'B')
+C = sum_matrices(A, B, size)
+print("Matrix:")
+for row in C:
+    print(row)
