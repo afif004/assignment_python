efbf7dc44e4e5f3c3d66efa419a9ca7dd4b26ef6
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-07 01:25:56 +0600

    Update unique_elements.py

--- array/unique_elements.py
+++ array/unique_elements.py
@@ -1,12 +1,9 @@
 N = int(input())
 arr =[]
-frequency = {}
+uniq = []
 for i in range(0,N):
     arr.append(int(input()))
-    if arr[i] in frequency:
-            frequency[arr[i]] += 1
-    else:
-            frequency[arr[i]] = 1
-for elmnt in frequency:
-    if frequency[elmnt]:
-        print(elmnt)
+    if not arr[i] in uniq:
+            uniq.append(arr[i])
+for elmnt in uniq:
+    print(elmnt)
