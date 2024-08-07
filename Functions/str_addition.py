25e45b9525596b7a40a7112426efe82b9d205b33
Mohammad Didarul Anowar Afif <147996649+afif004@users.noreply.github.com>
2024-08-08 01:59:32 +0600

    str

---
+++ Functions/str_addition.py
@@ -0,0 +1,21 @@
+def addition(s1,s2):
+    s1 = s1[::-1]
+    s2 = s2[::-1]
+    s3 = ""
+    carry = 0
+    for i in range (len(s1)):
+        d1 = int(s1[i])
+        d2 = int(s2[i]) if i < len(s2) else 0
+        total = d1 + d2 + carry
+        carry = total//10
+        s3 += str(total%10)
+    if carry:
+        s3 += str(carry)
+    s3 = s3[::-1]
+    return s3
+s1 = input()
+s2 = input()
+if len(s2) > len(s1):
+     s1, s2 = s2, s1
+s3 = addition(s1,s2)
+print(s3)
